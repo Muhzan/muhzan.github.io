38a88663f8b84fc5aca41af5a69a16287ff887c9
Muhzan <muhammadfauzanbaihaqi@gmail.com>
2024-03-30 06:35:29 +0700

    pantaiseaju

---
+++ Assets/Assets/js/script.js
@@ -0,0 +1,33 @@
+const navbar = document.getElementsByTagName('nav')[0];
+
+window.addEventListener('scroll', function() {
+    console.log(this.window.scrollY);
+
+    if (this.window.scrollY > 1) {
+        navbar.classList.replace('bg-transparent', 'navbar-color');
+    } else if (this.window.scrollY <= 0 ) {
+        navbar.classList.replace('navbar-color', 'bg-transparent');
+    }
+    
+});
+
+var navLinks = document.querySelectorAll('.nav-link ');
+
+window.addEventListener('scroll', function () {
+    var currentScrollPos = window.scrollY;
+
+    document.querySelectorAll('section').forEach(function (section, index) {
+        var sectionTop = section.offsetTop - 72; 
+        var sectionBottom = sectionTop + section.offsetHeight;
+
+        if (currentScrollPos >= sectionTop && currentScrollPos < sectionBottom) {
+
+            navLinks.forEach(function (link) {
+            link.classList.remove('active');
+            });
+
+            navLinks[index].classList.add('active');
+        }
+    });
+});
+
